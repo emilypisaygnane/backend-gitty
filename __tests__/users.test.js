ac0747120eced774ee4f2861c60a9c34be4dc68f
Emily Pisaygnane <episaygnane@me.com>
2022-12-05 15:04:31 -0800

    created bare bones framework, created new files for controllers, middleware, models, and services. Added app routes in app.js, renamed app.test file to users.test, added correct env data, and created tables and ran setup

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,9 +1,9 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
