de937d65824ad3daa89ea45e8c0234de6636d763
Emily Pisaygnane <episaygnane@me.com>
2022-12-05 17:39:01 -0800

    tests pass for github tests

---
+++ __tests__/github.test.js
@@ -0,0 +1,38 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/github');
+
+describe('github auth', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('/api/v1/github/login should redirect to the github oauth page', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(
+        `https://github.com/login/oauth/authorize?client_id=${process.env.GH_CLIENT_ID}&scope=user&redirect_uri=${process.env.GH_REDIRECT_URI}`
+        );
+  });
+
+  it('/api/v1/github/callback should login users and redirect to dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      login: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+});
